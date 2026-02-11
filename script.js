aa5d31ef1ef44f35c175ac7996bcebdcca2e0f77
KRINA CHANDRAKANT NAYAK <nayakkrina30@gmail.com>
2026-02-11 09:48:16 +0530

    Add event listener for DOMContentLoaded

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function() {
   const cards = document.querySelectorAll(".gallery .card img");
   const lightbox = document.getElementById("lightbox");
